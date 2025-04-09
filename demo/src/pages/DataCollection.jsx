e27f1454a023fd4ae8ba783239a6c99721d6c350
MOAZZEM HOSEN SARDAR <moazzemhosen9@gmail.com>
2025-04-09 13:32:55 +0530

    final modification

--- demo/src/pages/DataCollection.jsx
+++ demo/src/pages/DataCollection.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import DatacollectionCard from '../components/DatacollectionCard'
 import Float from './Float'
+import bgImage from '../assets/image55.jpg'
 
 const DataCollection = () => {
   return (
-    <div className="bg-gray-100 bg-[url('image55.jpg')] bg-cover bg-center bg-no-repeat min-h-screen" >
+    <div 
+    style={{backgroundImage:`url(${bgImage})`}}
+    className="bg-gray-100 bg-cover bg-center bg-no-repeat min-h-screen" >
     <Navbar />
     <div className="pt-33">
       {/*  for background- <h1 className="bg-gray-100 min-h-screen mt-33 bg-[url('image33.jpg')] bg-cover bg-center bg-no-repeat min-h-screen"></h1> */}
@@ -16,4 +19,4 @@ const DataCollection = () => {
   )
 }
 
-export default DataCollection
+export default DataCollection;
